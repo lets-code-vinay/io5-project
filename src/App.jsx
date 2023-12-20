8668e0e6416d48947f4ed30a8404b09c8c25c5a4
Shekhar <io4.shekhar@gmail.com>
2023-12-20 18:23:08 +0530

    signup-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./App.css";
 import SubHeader from "./components/SubHeader";
 import Header from "./components/Header";
-
+// import Login from "../src/components/Login/index.jsx";
+import Signup from "./components/signup/index.jsx";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
       <SubHeader />
+      {/* <Login />; */}
+      <Signup />
     </React.Fragment>
   );
 }
